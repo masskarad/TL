f1d16ea62d6679e6f30b33beb4067bd27fd681f0
Masskarad <masskaarad@gmail.com>
2016-11-07 14:52:11 +0100

    Ninja

---
+++ test/model/tache/CategorieTest.java
@@ -0,0 +1,53 @@
+package model.tache;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CategorieTest
+{
+
+	@Test
+	public void testAjouterNouvelleCategorie()
+	{
+		
+	}
+	
+	@Test
+	public void testAjouterCategorieExistante()
+	{
+		
+	}
+	
+	@Test
+	public void testAjouterMajuscule()
+	{
+		
+	}
+	
+	@Test
+	public void testSupprimerExistant()
+	{
+		
+	}
+	
+
+	@Test
+	public void testSupprimerNonExistant()
+	{
+		
+	}
+	
+	@Test
+	public void testModifierExistant()
+	{
+		
+	}
+	
+
+	@Test
+	public void testModifierNonExistant()
+	{
+		
+	}
+}
