4d28648c4323e85bc294f5b4551596ef856a8ca4
Louis <louisr.daviaud@gmail.com>
2016-11-10 14:54:58 +0100

    Update ListeTacheSimple.java

--- src/model/tache/liste_tache/ListeTacheSimple.java
+++ src/model/tache/liste_tache/ListeTacheSimple.java
@@ -1,38 +1,31 @@
 package model.tache.liste_tache;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.TreeSet;
 
 import model.tache.Tache;
+import model.tache.TachePonctuelle;
 
-public final class ListeTacheSimple implements Comparator<Tache>
+public final class ListeTacheSimple extends ListeTache
 {
-	private List<Tache> liste_taches;
-	
-	public ListeTacheSimple()
-	{
-		liste_taches = new ArrayList<Tache>();
-	}
-	
-	public ListeTacheSimple(Tache[] p_liste_taches)
-	{
-		liste_taches = Arrays.asList(p_liste_taches);
-	}
-	
-	public void trier()
-	{
-		liste_taches.sort(this);
-	}
-	
 	@Override
 	public int compare(Tache p_tache_a, Tache p_tache_b)
 	{
-		return (int) (p_tache_a.getDate_limite().getMillis() - p_tache_b.getDate_limite().getMillis());
+		return (int) p_tache_a.getDate_limite().getMillis() - (int) p_tache_b.getDate_limite().getMillis();
 	}
-	
+
 	public static void main(String [] Args)
 	{
+		Tache t1 = new TachePonctuelle("Yolo");
+		Tache t2 = new TachePonctuelle("Tarace");
+		Tache t3 = new TachePonctuelle("Azerrze");
+		Tache t4 = t3;
+		ListeTacheSimple liste_tache_simple = new ListeTacheSimple();
+		liste_tache_simple.add(t1);
+		liste_tache_simple.add(t2);
+		//liste_tache_simple.add(t3);
+		liste_tache_simple.add(t3);
+		liste_tache_simple.add(t4);
+		System.out.println(liste_tache_simple.toString());
+		System.out.println("Taille : " + liste_tache_simple.size());
 	}
 }
