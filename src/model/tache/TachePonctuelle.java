944ccda9d2e92d5fbb7a4f2e2644363250f86b12
Louis <louisr.daviaud@gmail.com>
2016-11-14 09:58:06 +0100

    Update TachePonctuelle.java

--- src/model/tache/TachePonctuelle.java
+++ src/model/tache/TachePonctuelle.java
@@ -5,10 +5,25 @@ import org.joda.time.DateTime;
 public final class TachePonctuelle extends Tache
 {
 
-	public TachePonctuelle(DateTime p_date_limite)
+	public Tache()
+	{
+		super();
+	}
+	
+	public Tache(String p_titre)
+	{
+		super(p_titre);
+	}
+	
+	public Tache(DateTime p_date_limite)
 	{
 		super(p_date_limite);
 	}
+	
+	public Tache(String p_titre, String p_categorie, DateTime p_date_limite)
+	{
+		super(p_titre, p_categorie, p_date_limite);
+}
 	
 	public boolean estEnRetard()
 	{
