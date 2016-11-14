5de1d3c3b44a9683475909330189a2e28f3497df
Louis <louisr.daviaud@gmail.com>
2016-11-14 14:34:55 +0100

    Update ListeTacheSimple.java

--- src/model/tache/liste_tache/ListeTacheSimple.java
+++ src/model/tache/liste_tache/ListeTacheSimple.java
@@ -1,30 +1,33 @@
 package model.tache.liste_tache;
 
-import java.util.TreeSet;
-
 import model.tache.Tache;
 import model.tache.TachePonctuelle;
 
 public final class ListeTacheSimple extends ListeTache
 {
-	@Override
-	public int compare(Tache p_tache_a, Tache p_tache_b)
-	{
-		return (int) p_tache_a.getDate_limite().getMillis() - (int) p_tache_b.getDate_limite().getMillis();
-	}
 
-	public static void main(String [] Args)
+	public static void main(String [] Args) throws Exception
 	{
 		Tache t1 = new TachePonctuelle("Yolo");
 		Tache t2 = new TachePonctuelle("Tarace");
 		Tache t3 = new TachePonctuelle("Azerrze");
-		Tache t4 = t3;
-		ListeTacheSimple liste_tache_simple = new ListeTacheSimple();
+		Tache t4 = new TachePonctuelle("We");
+		Tache t5 = t3;
+		Tache t6 = new TachePonctuelle("We");
+		ListeTache liste_tache_simple = new ListeTacheSimple();
 		liste_tache_simple.add(t1);
+		Thread.sleep(150);
 		liste_tache_simple.add(t2);
-		//liste_tache_simple.add(t3);
+		Thread.sleep(260);
 		liste_tache_simple.add(t3);
+		Thread.sleep(370);
+		liste_tache_simple.add(t3);
+		Thread.sleep(480);
 		liste_tache_simple.add(t4);
+		Thread.sleep(590);
+		liste_tache_simple.add(t5);
+		Thread.sleep(600);
+		liste_tache_simple.add(t6);
 		System.out.println(liste_tache_simple.toString());
 		System.out.println("Taille : " + liste_tache_simple.size());
 	}
