fa1711b1852b4fed779d382db628c618b6d39b99
Louis <louisr.daviaud@gmail.com>
2016-11-21 12:08:37 +0100

    add TLTest annots

--- src/model/tache/Tache.java
+++ src/model/tache/Tache.java
@@ -5,39 +5,42 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.Comparator;
+
+import annotation.TLTest;
+import annotation.TLTest.STATUS;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTime.Property;
-import org.joda.time.format.DateTimeFormat;
 
 public abstract class Tache implements Serializable, Comparable<Tache>
 {
 	private static final long serialVersionUID = -2452413728288347395L;
 
 	private final static String SANS_TITRE = "SANS_TITRE";
-	
+
 	private String titre;
 	private Categorie categorie;
 	private byte avencement;
 	private DateTime date_limite;
 	
+	@TLTest(status = STATUS.NOTEST)
 	public Tache()
 	{
 		titre       = SANS_TITRE;
 		categorie   = new Categorie();
 		avencement  = 0;
-		date_limite = DateTime.now().plusDays(1);
+		date_limite = DateTime.now().plus(1);
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public Tache(String p_titre)
 	{
 		titre       = p_titre;
 		categorie   = new Categorie();
 		avencement  = 0;
-		date_limite = DateTime.now().plusDays(1);
+		date_limite = DateTime.now().plus(1);
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public Tache(DateTime p_date_limite)
 	{
 		titre       = SANS_TITRE;
@@ -46,6 +49,7 @@ public abstract class Tache implements Serializable, Comparable<Tache>
 		date_limite = p_date_limite;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public Tache(String p_titre, String p_categorie, DateTime p_date_limite)
 	{
 		titre       = p_titre;
@@ -54,36 +58,43 @@ public abstract class Tache implements Serializable, Comparable<Tache>
 		date_limite = p_date_limite;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public final void setTitre(String p_nouveau_titre)
 	{
 		titre = p_nouveau_titre;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public final void setCategorie(String p_nouvelle_categorie)
 	{
 		categorie.setValeur(p_nouvelle_categorie);
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public final String getTitre()
 	{
 		return titre;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public final Categorie getCategorie()
 	{
 		return categorie;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public final int getAvencement()
 	{
 		return avencement;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public final DateTime getDate_limite()
 	{
 		return date_limite;
 	}
 	
+	@TLTest(status = STATUS.NOTEST)
 	public boolean valide()
 	{
 		//DateTimeFormat dtf = new DateTimeFormat();
@@ -92,18 +103,21 @@ public abstract class Tache implements Serializable, Comparable<Tache>
 	}
 	
 	@Override
+	@TLTest(status = STATUS.NOTEST)
 	public boolean equals(Object o)
 	{
 		return false;
 	}
 	
 	@Override
+	@TLTest(status = STATUS.NOTEST)
 	public int compareTo(Tache o)
 	{
 		return titre.compareTo(o.titre) + date_limite.compareTo(o.date_limite);
 	}
 	
 	@Override
+	@TLTest(status = STATUS.NOTEST)
 	public String toString()
 	{
 		StringBuilder string_builder = new StringBuilder();
@@ -114,13 +128,14 @@ public abstract class Tache implements Serializable, Comparable<Tache>
 		return string_builder.toString();
 	}
 	
+	@TLTest(status = STATUS.TOTEST)
 	public abstract boolean estEnRetard();
 	
 	public static void main(String [] Args)
 	{
 		try
 		{
-			Tache tache = new TacheAuLong(DateTime.now().minus(1),DateTime.now());
+			Tache tache = new TacheAuLong(DateTime.now());
 			tache.avencement = 42;
 			FileOutputStream fileOut = new FileOutputStream("/home/harkame/tache.ser");
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
